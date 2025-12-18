de6e1ac5a3f78c100124ddd3646c620f732beee3
Steven Ampie <ampiesteven24@gmail.com>
2025-12-18 11:29:55 -0500

    feat(Glass): added api main

---
+++ Proyectos_Hackathon/Pa10/Glass/api/api.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import SQLModel
+
+from db import engine
+import Proyectos_Hackathon.Pa10.Glass.api.tables
+
+healthy_station = FastAPI(
+    title="healthy station",
+    description="Final project SIC 2025",
+)
+
+healthy_station.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+healthy_station_v0 = APIRouter(prefix="/v0", tags=["v0"])
+
+SQLModel.metadata.create_all(bind=engine)
+
+
+
+@healthy_station.get("/")
+def root():
+    """Example endpoint."""
+    return {"message": "Welcome to Healthy Station"}
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app=healthy_station, host="0.0.0.0", port=8001)
